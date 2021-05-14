4a97dd5b63e58b2ff023a7d5b2baf488a4ac2deb
Brian Sahota <bpsahota@gmail.com>
2021-05-13 20:04:21 -0400

    started on nav

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+import Logo from '../assets/logo.svg'
 
 const Navbar = () => {
     return (
@@ -9,3 +12,27 @@ const Navbar = () => {
 }
 
 export default Navbar
+
+const NavContainer = styled.nav`
+height: 5rem;
+display: flex;
+align-items: center;
+justify-content: center;
+
+.nav-center {
+    width: 90vw;
+    margin: 0 auto;
+    max-width: var(--max-width)
+}
+.nav-header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    img {
+        width: 175px;
+        margin-left: -15px;
+    }
+
+}
+
+`
