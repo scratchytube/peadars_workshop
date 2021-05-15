04ea04d19d42465e19bf13d12187911bdf06a20e
Brian Sahota <bpsahota@gmail.com>
2021-05-15 11:03:32 -0400

    nav almost done

--- src/Components/CartButtons.js
+++ src/Components/CartButtons.js
@@ -6,18 +6,35 @@ import { FaShoppingCart, FaUserMinus, FaUserPlus } from 'react-icons/fa'
 
 const CartButtons = () => {
     return (
-        <div>
             <Wrapper className='cart-btn-wrapper'>
                 <Link to='/cart' className='cart-btn'>
-                    <FaShoppingCart />
+                    Cart 
+                    <span className='cart-container'>
+                        <FaShoppingCart />
+                        <span className='cart-value'>12</span>
+                    </span>
                 </Link>
+                <button type='button' className='auth-button'>
+                    Login <FaUserPlus />
+                </button>
             </Wrapper>
-        </div>
     )
 }
 
 export default CartButtons
 
 const Wrapper = styled.div`
+display: grid;
+grid-template-columns: 1fr 1fr;
+align-items: center;
+width: 225px;
+
+.cart-btn {
+    color: var(--clr-grey-1);
+    font-size: 1.5rem;
+    letter-spacing: var(--spacing);
+    display: flex;
+    align-items: center;
+  }
 
 `
