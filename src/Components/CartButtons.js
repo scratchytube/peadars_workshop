9f785ee098a96bd0f048ef5acb0ca1ce0d2ed72a
Brian Sahota <bpsahota@gmail.com>
2021-05-14 21:59:41 -0400

    nav wip

--- src/Components/CartButtons.js
+++ src/Components/CartButtons.js
@@ -1,11 +1,23 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+import { FaShoppingCart, FaUserMinus, FaUserPlus } from 'react-icons/fa'
+
 
 const CartButtons = () => {
     return (
         <div>
-            <h4>cart buttons</h4>
+            <Wrapper className='cart-btn-wrapper'>
+                <Link to='/cart' className='cart-btn'>
+                    <FaShoppingCart />
+                </Link>
+            </Wrapper>
         </div>
     )
 }
 
 export default CartButtons
+
+const Wrapper = styled.div`
+
+`
