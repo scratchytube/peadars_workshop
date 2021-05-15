a0cb9ff44eacd41ab455b95959bbea6547490a29
Brian Sahota <bpsahota@gmail.com>
2021-05-15 11:31:27 -0400

    nav css done

--- src/Components/CartButtons.js
+++ src/Components/CartButtons.js
@@ -35,6 +35,56 @@ width: 225px;
     letter-spacing: var(--spacing);
     display: flex;
     align-items: center;
+}
+.cart-container {
+    display: flex;
+    align-items: center;
+    position: relative;
+    svg {
+        height: 1.6rem
+        margin-left: 5px
+    }
+}
+.cart-value {
+    position: absolute;
+    top: -10px;
+    right: -16px;
+    background: var(--clr-primary-5);
+    width: 16px;
+    height: 16px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 50%;
+    font-size: 0.75rem;
+    color: var(--clr-white);
+    padding: 12px;
   }
+// .auth-btn {
+//     display: flex;
+//     align-items: center;
+//     background: transparent;
+//     border-color: transparent;
+//     font-size: 1.5rem;
+//     cursor: pointer;
+//     color: var(--clr-grey-1);
+//     letter-spacing; var(--spacing);
+//     svg {
+//         margin-left: 5px;
+//     }
+// }
 
+.auth-button {
+    display: flex;
+    align-items: center;
+    background: transparent;
+    border-color: transparent;
+    font-size: 1.5rem;
+    cursor: pointer;
+    color: var(--clr-grey-1);
+    letter-spacing: var(--spacing);
+    svg {
+      margin-left: 5px;
+    }
+  }
 `
