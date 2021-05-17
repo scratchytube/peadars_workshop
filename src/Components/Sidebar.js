47a7389b2fc7114e03dec21a855d35b538cd8f71
Brian Sahota <bpsahota@gmail.com>
2021-05-17 11:51:03 -0400

    css almost done with sidebar

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
 import CartButtons from './CartButtons'
 import styled from 'styled-components'
@@ -6,10 +6,10 @@ import logo from '../assets/logo.svg'
 import { FaTimes } from 'react-icons/fa'
 
 const Sidebar = () => {
-    const [ isOpen ] = useState(true)
+    const isOpen = false
     return (
         <SidebarContainer>
-            <aside className={isOpen ? `sidebar show-sidebar` : 'sidebar'}>
+            <aside className={`${isOpen ? `sidebar show-sidebar` : 'sidebar'}`}>
                 <div className="sidebar-header">
                     <img src={logo} className="logo" alt="peaders workshop" />
                     <button className="close-btn" type='button'>
@@ -38,34 +38,145 @@ const Sidebar = () => {
 
 export default Sidebar
 
-const SidebarContainer = styled.div`
-text-align: center;
-.sidebar-header {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 1rem 1.5rem;
-}
-.close-btn {
-    font-size: 2rem;
-    background: transparent;
-    border-color: transparent;
-    color: var(--clr-primary-5);
-    transition: var(--transition);
-    cursor: pointer;
-    color: var(--clr-red-dark);
-    margin-top: 0.2rem;
-}
-.close-btn:hover {
-    color: var(--clr-red-light)
-}
-.logo {
-    justify-self: center;
-    height: 160px;
-}
-.links {
-    margin-bottom: 2rem;
-}
-`
+// const SidebarContainer = styled.div`
+// text-align: center;
+// .sidebar-header {
+//     display: flex;
+//     justify-content: space-between;
+//     align-items: center;
+//     padding: 1rem 1.5rem;
+// }
+// .close-btn {
+//     font-size: 2rem;
+//     background: transparent;
+//     border-color: transparent;
+//     color: var(--clr-primary-5);
+//     transition: var(--transition);
+//     cursor: pointer;
+//     color: var(--clr-red-dark);
+//     margin-top: 0.2rem;
+// }
+// .close-btn:hover {
+//     color: var(--clr-red-light)
+// }
+// .logo {
+//     justify-self: center;
+//     height: 160px;
+// }
+// .links {
+//     margin-bottom: 2rem;
+// }
+// .links a {
+//     display: block;
+//     text-align: left;
+//     font-size: 1rem;
+//     text-transform: capitalize;
+//     padding: 1rem 1.5rem;
+//     color: var(--clr-grey-3);
+//     transition: var(--transition);
+//     letter-spacing: var(--spacing);
+// }
+// .links a:hover {
+//     padding: 1rem 1.5rem
+//     padding-left: 2rem;
+//     background: var(--clr-grey-10);
+//     color: var(--clr-grey-2);
+// }
+
+// .sidebar {
+//     position: fixed;
+//     top: 0;
+//     left: 0;
+//     width: 100%;
+//     height: 100%;
+//     background: var(--clr-white);
+//     transition: var(--transition);
+//     transform: translate(-100%);
+//     z-index: -1;
+// }
+// .show-sidebar {
+//     transform: translate(0)
+//     z-index: 999;
+// }
+// .cart-btn-wrapper {
+//     margin: 2rem auto
+// }
+// @media screen and (min-width: 992px) {
+//     .sidebar {
+//         display: none;
+//     }
+// }
+// `
+
+// const SidebarContainer = styled.div`
+//   text-align: center;
+//   .sidebar-header {
+//     display: flex;
+//     justify-content: space-between;
+//     align-items: center;
+//     padding: 1rem 1.5rem;
+//   }
+//   .close-btn {
+//     font-size: 2rem;
+//     background: transparent;
+//     border-color: transparent;
+//     color: var(--clr-primary-5);
+//     transition: var(--transition);
+//     cursor: pointer;
+//     color: var(--clr-red-dark);
+//     margin-top: 0.2rem;
+//   }
+//   .close-btn:hover {
+//     color: var(--clr-red-light);
+//   }
+//   .logo {
+//     justify-self: center;
+//     height: 45px;
+//   }
+//   .links {
+//     margin-bottom: 2rem;
+//   }
+//   .links a {
+//     display: block;
+//     text-align: left;
+//     font-size: 1rem;
+//     text-transform: capitalize;
+//     padding: 1rem 1.5rem;
+//     color: var(--clr-grey-3);
+//     transition: var(--transition);
+//     letter-spacing: var(--spacing);
+//   }
+
+//   .links a:hover {
+//     padding: 1rem 1.5rem;
+//     padding-left: 2rem;
+//     background: var(--clr-grey-10);
+//     color: var(--clr-grey-2);
+//   }
+
+//   .sidebar {
+//     position: fixed;
+//     top: 0;
+//     left: 0;
+//     width: 100%;
+//     height: 100%;
+//     background: var(--clr-white);
+//     transition: var(--transition);
+//     transform: translate(-100%);
+//     z-index: -1;
+//   }
+//   .show-sidebar {
+//     transform: translate(0);
+//     z-index: 999;
+//   }
+//   .cart-btn-wrapper {
+//     margin: 2rem auto;
+//   }
+//   @media screen and (min-width: 992px) {
+//     .sidebar {
+//       display: none;
+//     }
+//   }
+// `
 
 
