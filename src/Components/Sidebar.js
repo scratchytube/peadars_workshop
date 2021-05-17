944d21a7c78e43c45ccd015e656ea01526a9a38c
Brian Sahota <bpsahota@gmail.com>
2021-05-17 10:29:13 -0400

    sidebar skeleton done

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,10 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import CartButtons from './CartButtons'
 import styled from 'styled-components'
+import logo from '../assets/logo.svg'
+import { FaTimes } from 'react-icons/fa'
 
 const Sidebar = () => {
+    const [ isOpen ] = useState(true)
     return (
         <SidebarContainer>
-            <h4>sidebih</h4>
+            <aside className={isOpen ? `sidebar show-sidebar` : 'sidebar'}>
+                <div className="sidebar-header">
+                    <img src={logo} className="logo" alt="peaders workshop" />
+                    <button className="close-btn" type='button'>
+                        <FaTimes />
+                    </button>
+                </div>
+                <ul className="links">
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to='/about'>About</Link>
+                    </li>
+                    <li>
+                        <Link to='/products'>The Goods</Link>
+                    </li>
+                    <li>
+                        <Link to='/checkout'>Checkout</Link>
+                    </li>
+                </ul>
+                <CartButtons />
+            </aside>
         </SidebarContainer>
     )
 }
