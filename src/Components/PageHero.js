db1fcf58aaa0e4f987db3482afbee5b234f9f594
Brian Sahota <bpsahota@gmail.com>
2021-05-25 10:41:21 -0400

    pagehero css done

--- src/Components/PageHero.js
+++ src/Components/PageHero.js
@@ -7,9 +7,7 @@ const PageHero = ({ title }) => {
         <Wrapper>
             <div className="section-center">
                 <h3>
-                    <Link to='/'>/ {title}
-                    Home
-                    </Link>
+                    <Link to='/'>Home</Link>/ {title}
                 </h3>
             </div>
         </Wrapper>
@@ -20,5 +18,19 @@ export default PageHero
 
 const Wrapper = styled.section`
 background: var(--clr-primary-10);
+width: 100%;
+min-height: 20vh;
+display: flex;
+align-items: center;
+
+color: var(--clr-primary-1);
+a {
+    color: var(--clr-primary-3);
+    padding: 0.5rem;
+    transition: var(--transition);
+}
+a:hover {
+    color: var(--clr-primary-1);
+}
 
 `
