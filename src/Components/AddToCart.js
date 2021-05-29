6dfe1affeeb2ab2a89c2ea5e169705e50dc1c797
Brian Sahota <bpsahota@gmail.com>
2021-05-29 16:26:11 -0400

    css done for amount buttons

--- src/Components/AddToCart.js
+++ src/Components/AddToCart.js
@@ -51,5 +51,39 @@ const AddToCart = ({ product }) => {
 export default AddToCart
 
 const Wrapper = styled.section`
-margin-top
+margin-top: 2rem;
+// div {
+//     display: flex;
+// }
+// .btn-container {
+//     margin-top: 2rem;
+// }
+.btn {
+    margin-top: 1rem;
+    width: 140px;
+}
+.amount-btns {
+    display: grid;
+    width: 140px;
+    justify-items: center;
+    grid-template-columns: repeat(3, 1fr);
+    align-items: center;
+    h2 {
+        margin-bottom: 0
+    }
+}
+button {
+    background: transparent;
+    border-color: transparent;
+    cursor: pointer;
+    padding: 1rem 0;
+    width: 2rem;
+    height: 1rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+h2 {
+    margin-bottom: 0
+}
 `
