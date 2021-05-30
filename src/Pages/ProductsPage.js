582b6517b8ac96f4e84599396468a3adb2042e0b
Brian Sahota <bpsahota@gmail.com>
2021-05-30 11:27:34 -0400

    products css done

--- src/Pages/ProductsPage.js
+++ src/Pages/ProductsPage.js
@@ -22,5 +22,14 @@ const ProductsPage = () => {
 export default ProductsPage
 
 const Wrapper = styled.div`
-
+.products {
+    display: grid;
+    gap: 3rem 1.5rem;
+    margin: 4rem auto;
+}
+@media (min-width: 768px) {
+    .products {
+        grid-template-columns: 200px 1fr;
+    }
+}
 `
