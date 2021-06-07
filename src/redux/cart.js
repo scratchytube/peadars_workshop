a9275f83288304e10e0e79d039d5473b24ec39e9
Brian Sahota <bpsahota@gmail.com>
2021-06-07 13:02:43 -0400

    done with cart array in redux

--- src/redux/cart.js
+++ src/redux/cart.js
@@ -12,7 +12,19 @@ const cartSlice = createSlice({
             const { id, amount, product } = action.payload
             const tempItem = state.cart.find((i) => i.id === id )
             if (tempItem) {
+                const tempCart = state.cart.map((cartItem) => {
+                    if (cartItem.id === id) {
+                        let newAmount = cartItem.amount + amount
+                        if(newAmount > cartItem.max) {
+                            newAmount = cartItem.max
+                        }
+                        return {...cartItem, amount: newAmount} 
+                    } else {
+                        return cartItem
+                    }
+                } )
 
+                return {...state, cart: tempCart}
             } else {
                 const newItem = {id, name: product.name, amount, image: product.images[0].url, price: product.price, max: product.stock }
                 return {...state,cart:[...state.cart, newItem]}
