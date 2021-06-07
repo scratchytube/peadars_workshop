9d1b17c533b5eeb179842d8666768e5f564a4fca
Brian Sahota <bpsahota@gmail.com>
2021-06-07 12:07:40 -0400

    added cart reducer

--- src/redux/cart.js
+++ src/redux/cart.js
@@ -8,9 +8,16 @@ const cartSlice = createSlice({
         totalAmount: 0,
     },
     reducers: {
-        // addToCart: (state, action) {
+        addToCart(state, action) {
+            const { id, amount, product } = action.payload
+            const tempItem = state.cart.find((i) => i.id === id )
+            if (tempItem) {
 
-        // }
+            } else {
+                const newItem = {id, name: product.name, amount, image: product.images[0].url, price: product.price, max: product.stock }
+                return {...state,cart:[...state.cart, newItem]}
+            }
+        }
     }
 })
 
