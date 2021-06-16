6fec82347f4a026c7081cef9af1513ccc9135579
Brian Sahota <bpsahota@gmail.com>
2021-06-16 16:46:02 -0400

    added auth0 again

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
+import { Auth0Provider } from "@auth0/auth0-react"
 import store from './redux/store'
 import './index.css';
 import App from './App';
@@ -9,7 +10,14 @@ import App from './App';
 ReactDOM.render(
         <BrowserRouter>
             <Provider store={store}>
-                <App />
+            <Auth0Provider
+                domain="brian-patrick.us.auth0.com"
+                clientId="230Hp2cnbMsdsMKxWV5jL4EzWNLR3P9u"
+                redirectUri={window.location.origin}
+                cacheLocation='localStorage'
+                >
+                    <App />
+                </Auth0Provider>
             </Provider>
         </BrowserRouter>
     ,document.getElementById('root')
