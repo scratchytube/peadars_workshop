4a47108cdd575aeef12717a64dbf1d8644bb17e5
Brian Sahota <bpsahota@gmail.com>
2021-06-17 07:40:06 -0400

    lol removed auth0 again

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import { Auth0Provider } from "@auth0/auth0-react"
 import store from './redux/store'
 import './index.css';
 import App from './App';
@@ -10,14 +9,7 @@ import App from './App';
 ReactDOM.render(
         <BrowserRouter>
             <Provider store={store}>
-            <Auth0Provider
-                domain="brian-patrick.us.auth0.com"
-                clientId="230Hp2cnbMsdsMKxWV5jL4EzWNLR3P9u"
-                redirectUri={window.location.origin}
-                cacheLocation='localStorage'
-                >
-                    <App />
-                </Auth0Provider>
+                <App />
             </Provider>
         </BrowserRouter>
     ,document.getElementById('root')
