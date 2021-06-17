27f6ba77d34eff28d1bfb9d8549aca5de66925c5
Brian Sahota <bpsahota@gmail.com>
2021-06-17 10:28:34 -0400

    done with login component

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 
 const Login = () => {
@@ -21,25 +22,29 @@ const Login = () => {
             <div className="login">
                 <form onSubmit={handleSubmit} autoComplete='off' >
                     <h2>Welcome Back!</h2>
-                    {/* <label>Username</label> */}
-                    <input 
-                    className='field-input'
-                    placeholder="Username"
-                    type="text"
-                    name="username"
-                    value={formData.username}
-                    onChange={handleChange} 
-                    />
-                    {/* <label>Password</label> */}
-                    <input
-                    className='field-input' 
-                    placeholder="Password"
-                    type="password"
-                    name="password"
-                    value={formData.password}
-                    onChange={handleChange} 
-                    />
+                    <div className='input-div' >
+                        <input 
+                        className='field-input'
+                        placeholder="Username"
+                        type="text"
+                        name="username"
+                        value={formData.username}
+                        onChange={handleChange} 
+                        />
+                        <input
+                        className='field-input' 
+                        placeholder="Password"
+                        type="password"
+                        name="password"
+                        value={formData.password}
+                        onChange={handleChange} 
+                        />
                     <input className='btn' type="submit" value="Login" />
+                    </div>
+                    <div className='login-links' >
+                        <p>Don't have an account? <Link>Register</Link></p>
+                        <Link>Forgot password?</Link>
+                    </div>
                 </form>
             </div>
         </Wrapper>
@@ -49,21 +54,32 @@ const Login = () => {
 export default Login
 
 const Wrapper = styled.div`
-div {
-    display:flex;
+
+// .login {
+//     width: 360px;
+//     padding: 8% 0 0;
+//     margin: auto;
+// }
+
+.login {
+    display: flex;
     justify-content: center;
     align-items: center;
+    width: 400px;
+    margin: auto;
 }
 
-form {
+
+.input-div {
+    margin-top: 1.5rem;
     display: flex;
-    flex-direction: column;
+    flex-direction: column; 
     justify-content: center;
     align-items: center;
 }
 
 .field-input {
-    margin-bottom: 1rem;
+    margin-bottom: 1.25rem;
     width: 250px;
     height: 2rem;
     padding: 0.5rem;
@@ -71,5 +87,13 @@ form {
     border-radius: var(--radius);
     border-color: transparent;
     letter-spacing: var(--spacing);
+}
+
+.login-links {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+    margin-top: 1rem;
 }
 `
