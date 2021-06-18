36be7de12fb6d34062cd1962667ef4d734d8957f
Brian Sahota <bpsahota@gmail.com>
2021-06-18 14:14:24 -0400

    made checkout a private route

--- src/App.js
+++ src/App.js
@@ -87,9 +87,9 @@ const App = () => {
           <Route exact path='/products/:id' 
           children={<SingleProduct />}/
           >
-          <Route exact path="/checkout">
+          <PrivateRoute exact path="/checkout">
             <Checkout />
-          </Route>
+          </PrivateRoute>
           <Route exact path="*">
             <Error />
           </Route>
--- src/Pages/PrivateRouter.js
+++ src/Pages/PrivateRouter.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import { Route, Redirect } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 
-const PrivateRouter = () => {
+const PrivateRouter = ({ children, ...rest }) => {
+    const user = useSelector(state => state.user.user)
+
     return (
-        <div>
-            <h4>private route drive</h4>
-        </div>
+        <Route {...rest} render={() => {
+            return user ? children : <Redirect to='/' ></Redirect>
+        }}
+        ></Route>
     )
 }
 
