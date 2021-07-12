028ffa55f82fd8d7fa865ec16cc9e7f805aea6dc
Brian Sahota <bpsahota@gmail.com>
2021-07-12 10:53:40 -0400

    new page scrolls to top now

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
+import ScrollTopTop from '../src/Components/ScrollToTop'
 import { Provider } from 'react-redux'
 import store from './redux/store'
 import './index.css';
@@ -8,6 +9,7 @@ import App from './App';
 
 ReactDOM.render(
         <BrowserRouter>
+            <ScrollTopTop />
             <Provider store={store}>
                 <App />
             </Provider>
